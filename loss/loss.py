b742b2408eddbc0c89746f89b4b34a19d56d0998
Hyo Jun Jung <junbug331@gmail.com>
2022-04-19 09:41:22 +0900

    added loss module

---
+++ loss/loss.py
@@ -0,0 +1,20 @@
+import sys
+import torch
+import torch.nn as nn
+
+class MNISTloss(nn.Module):
+    def __init__(self, device=torch.device('cpu')):
+        super(MNISTloss, self).__init__()
+        self.loss = nn.CrossEntropyLoss().to(device)
+    
+    def forward(self, out, gt):
+        loss_val = self.loss(out, gt)
+        return loss_val
+
+def get_criterion(crit="mnist", device=torch.device('cpu')):
+    if crit is "mnist":
+        return MNISTloss(device=device)
+    else:
+        print("unknown criterion")
+        sys.exit()
+        return
